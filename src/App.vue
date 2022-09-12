aa0570b70a902dd9133813d311c43aea6da2fe28
RudenokArt <rudenokart@yandex.ru>
2022-09-12 10:54:52 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
-  items-list
+  <div class="container pt-5">
+    <div class="row justyfy-content-between mt-1 border" v-for="(item, index) in $store.state.itemsList">
+      <div class="col-8 p-1">
+        {{item.product}}
+      </div>
+      <div class="col-1">
+        <button class="btn btn-sm btn-outline-success">
+          <i class="fa fa-check" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    {{$store.state.itemsList}}
+  </div>
 </template>
 
 <script>
 
-export default {
+  export default {
 
-}
+    methods: {
+
+
+    },
+
+    mounted: function () {
+      this.$store.commit('GetItemsList');
+    }
+
+  }
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    apiUrl: 'http://e25532i4.beget.tech/shopping-list/api/?',
+    itemsList: [],
   },
   mutations: {
+    GetItemsList: async function () {
+      var itemsList = await fetch(this.state.apiUrl+'get_shopping_list=Y').then(function (response){
+        return response.text();
+      }).then(function(text){
+        return JSON.parse(text);
+      });
+      this.state.itemsList = itemsList;
+    },
+  },
+  getters: {
   },
   actions: {
   },
